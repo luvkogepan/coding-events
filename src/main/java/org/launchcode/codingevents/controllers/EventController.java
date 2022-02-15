363106675da623837e87b193263f3f87340d59fb
Michelle Ferris <jmichellelim@yahoo.com>
2022-02-15 12:52:18 -0500

    init

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,40 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    private static List<String> events = new ArrayList<>();
+
+    @GetMapping
+    public String displayAllEvents(Model model) {
+//        events.add("Code with Pride");
+//        events.add("Strange Loop");
+//        events.add("Apple WWDC");
+//        events.add("SpringOne Platform");
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+
+    @GetMapping("create")
+    public String renderCreateEventForm() {
+        return "events/create";
+    }
+    //lives at same place as above getmapping request. ok because one is get and one is post
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        //redirects to root path(displayAllEvents)
+        return "redirect:";
+    }
+}
