7ead725029907f943b52c899f4b6e9830fdf88e0
Michelle Ferris <jmichellelim@yahoo.com>
2022-02-15 18:35:30 -0500

    mostly finished exercise

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,22 +8,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+//    private static List<String> events = new ArrayList<>();
+    private HashMap<String, String> events = new HashMap<>();
+
+//    @GetMapping
+//    public String displayAllEvents(Model model) {
+////        events.add("Code with Pride");
+////        events.add("Strange Loop");
+////        events.add("Apple WWDC");
+////        events.add("SpringOne Platform");
+//        model.addAttribute("events", events);
+//        return "events/index";
+//    }
 
     @GetMapping
     public String displayAllEvents(Model model) {
-//        events.add("Code with Pride");
-//        events.add("Strange Loop");
-//        events.add("Apple WWDC");
-//        events.add("SpringOne Platform");
-        model.addAttribute("events", events);
-        return "events/index";
+       events.put("WWDC", "Apple convention");
+       events.put("Developer Week", "Developer conference");
+       events.put("Cisco Live", "Cisco convention");
+       model.addAttribute("events", events);
+       return "events/index";
     }
 
     @GetMapping("create")
@@ -33,7 +44,8 @@ public class EventController {
     //lives at same place as above getmapping request. ok because one is get and one is post
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+//        events.add(eventName);
+        events.put("name", "description");
         //redirects to root path(displayAllEvents)
         return "redirect:";
     }
